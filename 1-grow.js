991972afb3415b3b511a76ee430fe0e7ee0dd157
trick2001 <willemlouis99@gmail.com>
2024-01-23 21:32:09 +0800

    day 1/366

---
+++ 1-grow.js
@@ -0,0 +1,19 @@
+// Cara Pertama
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result *= x[i];
+//   }
+//   return result;
+// }
+
+// Cara Kedua
+// function grow(x) {
+//   const result = x.reduce((acc, curr) => acc * curr, 1);
+//   return result;
+// }
+
+// Cara Ketiga
+const grow = (x) => x.reduce((acc, curr) => acc * curr);
+
+console.log(grow([2, 2, 2, 2, 2, 2]));
