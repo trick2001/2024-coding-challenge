c237733941f3ff25b315d84cb411c70821741ada
trick2001 <willemlouis99@gmail.com>
2024-02-27 09:41:24 +0800

    day #3

---
+++ 3-hero.js
@@ -0,0 +1,19 @@
+// WPU Coding Challenge 2024
+// Day 3/366
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+// function hero(bullets, dragons) {
+//   if (bullets / 2 >= dragons) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function hero(bullets, dragons) {
+//   return bullets / 2 >= dragons;
+// }
+
+const hero = (bullets, dragons) => bullets / 2 >= dragons;
+
+console.log(hero(10, 5));
